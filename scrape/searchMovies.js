76c1f97b188b3db372f5c31516a491111979069f
donjoe <giumontanaro@gmail.com>
2021-01-10 17:14:06 +0100

    Scrape searchresults page

---
+++ scrape/searchMovies.js
@@ -0,0 +1,33 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const baseUrl = 'https://www.imdb.com/find?q='
+
+const getMovies = async (query) => {
+    const { data } = await axios.get(`${baseUrl}${query}`)
+
+    const $ = cheerio.load(data)
+
+    const movies = []
+    $('.findSection')
+        .first()
+        .find('.findResult')
+        .each((i, el) => {
+            const elem = $(el)
+            const title = elem.find('.result_text a')
+            const img = elem.find('img')
+            const extraInfo = elem.find('.result_text').text().match(/\((.*)\)/)[0] //extract everything inside ()
+            const movieID = title.attr('href').match(/title\/(.*)\//)[1] //extract movieid from href after title/
+
+            movies.push({
+                title: title.text(),
+                img: img.attr('src'),
+                extraInfo,
+                movieID
+            })
+        })
+
+        return movies
+}
+
+getMovies('back to the future')
