88bf96dfeab41026540acbb6520d2c0124097b24
donjoe <giumontanaro@gmail.com>
2021-01-10 20:58:00 +0100

    Create an express /movie route that serves a json movie

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const getMovies = require('./scrape/searchMovies')
+const getMovie = require('./scrape/scrapeMovie')
 
 const app = express()
 
@@ -8,6 +9,13 @@ app.get('/search/:movie', (req, res) => {
     getMovies(req.params.movie)
         .then(movies => {
             res.json(movies)
+        })
+})
+
+app.get('/movie/:id', (req, res) => {
+    getMovie(req.params.id)
+        .then(movie => {
+            res.json(movie)
         })
 })
 
